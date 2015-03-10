c6821cb15b876f5bba82940a1005771bafa22470
Tom Everett <tom@khubla.com>
2015-03-09 21:31:14 -0600

    acoustid lookup of musicbrainz id works

--- src/main/java/com/khubla/musicbrainztagger/AcoustID.java
+++ src/main/java/com/khubla/musicbrainztagger/AcoustID.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -29,17 +30,24 @@ public class AcoustID {
    /**
     * recording
     */
-   private class Recording {
+   private static class Recording {
       String id;
    }
 
    /**
     * result
     */
-   private class Result {
+   private static class Result {
       String id;
-      List<Recording> recordings;
+      List<Recording> recordings = new ArrayList<Recording>();
       String score;
+   }
+
+   /**
+    * results
+    */
+   private static class Results {
+      List<Result> results = new ArrayList<Result>();
    };
 
    /**
@@ -66,24 +74,34 @@ public class AcoustID {
    /**
     * get the musicbrainz id
     */
-   private static String getMusicBrainzID(String json) {
+   private static AcoustID.Results getResults(String json) {
       final JsonElement jelement = new JsonParser().parse(json);
       JsonObject jobject = jelement.getAsJsonObject();
       final JsonElement statusElement = jobject.get("status");
       if (statusElement.getAsString().compareTo("ok") == 0) {
+         final AcoustID.Results results = new AcoustID.Results();
          final JsonArray jarray = jobject.getAsJsonArray("results");
          /*
-          * use the first result
+          * walk the results
           */
-         jobject = jarray.get(0).getAsJsonObject();
-         final JsonElement resultIdElement = jobject.get("id");
-         final JsonArray recordingsArray = jobject.getAsJsonArray("recordings");
-         /*
-          * use the first recording
-          */
-         final JsonObject recordingJsonObject = recordingsArray.get(0).getAsJsonObject();
-         final JsonElement recordingIdElement = recordingJsonObject.get("id");
-         return recordingIdElement.getAsString();
+         for (int i = 0; i < jarray.size(); i++) {
+            final AcoustID.Result result = new AcoustID.Result();
+            jobject = jarray.get(i).getAsJsonObject();
+            result.id = jobject.get("id").getAsString();
+            result.score = jobject.get("score").getAsString();
+            final JsonArray recordingsArray = jobject.getAsJsonArray("recordings");
+            /*
+             * walk the recordings
+             */
+            for (int j = 0; j < recordingsArray.size(); j++) {
+               final AcoustID.Recording recording = new AcoustID.Recording();
+               final JsonObject recordingJsonObject = recordingsArray.get(j).getAsJsonObject();
+               recording.id = recordingJsonObject.get("id").getAsString();
+               result.recordings.add(recording);
+            }
+            results.results.add(result);
+         }
+         return results;
       } else {
          return null;
       }
@@ -101,7 +119,8 @@ public class AcoustID {
          final HttpEntity httpEntity = httpResponse.getEntity();
          final String json = EntityUtils.toString(httpEntity);
          System.out.println(json);
-         return getMusicBrainzID(json);
+         final AcoustID.Results results = getResults(json);
+         return results.results.get(0).recordings.get(0).id;
       } finally {
          httpResponse.close();
       }
