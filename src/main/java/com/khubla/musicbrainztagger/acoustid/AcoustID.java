85aa1f4df8392314f20633615b86eea24f1d4909
Tom Everett <tom@khubla.com>
2015-03-12 15:50:05 -0600

    results filter

--- src/main/java/com/khubla/musicbrainztagger/acoustid/AcoustID.java
+++ src/main/java/com/khubla/musicbrainztagger/acoustid/AcoustID.java
@@ -13,58 +13,88 @@ import com.khubla.musicbrainztagger.HTTPUtil;
 
 /**
  * simple wrapper for AcoustID
- *
+ * 
  * @author tom
  */
 public class AcoustID {
-   /**
-    * Chromaprint the file passed in
-    */
-   public static ChromaPrint chromaprint(File file, String fpcalc) throws IOException {
-      final ProcessBuilder processBuilder = new ProcessBuilder(fpcalc, null);
-      processBuilder.redirectErrorStream(true);
-      processBuilder.command().set(1, file.getAbsolutePath());
-      final Process fpcalcProc = processBuilder.start();
-      final BufferedReader br = new BufferedReader(new InputStreamReader(fpcalcProc.getInputStream()));
-      String line;
-      String chromaprint = null;
-      String duration = null;
-      while ((line = br.readLine()) != null) {
-         if (line.startsWith("FINGERPRINT=")) {
-            chromaprint = line.substring("FINGERPRINT=".length());
-         } else if (line.startsWith("DURATION=")) {
-            duration = line.substring("DURATION=".length());
-         }
-      }
-      return new ChromaPrint(chromaprint, duration);
-   }
+	/**
+	 * Chromaprint the file passed in
+	 */
+	public static ChromaPrint chromaprint(File file, String fpcalc)
+			throws IOException {
+		final ProcessBuilder processBuilder = new ProcessBuilder(fpcalc, null);
+		processBuilder.redirectErrorStream(true);
+		processBuilder.command().set(1, file.getAbsolutePath());
+		final Process fpcalcProc = processBuilder.start();
+		final BufferedReader br = new BufferedReader(new InputStreamReader(
+				fpcalcProc.getInputStream()));
+		String line;
+		String chromaprint = null;
+		String duration = null;
+		while ((line = br.readLine()) != null) {
+			if (line.startsWith("FINGERPRINT=")) {
+				chromaprint = line.substring("FINGERPRINT=".length());
+			} else if (line.startsWith("DURATION=")) {
+				duration = line.substring("DURATION=".length());
+			}
+		}
+		return new ChromaPrint(chromaprint, duration);
+	}
 
-   /**
-    * get the musicbrainz id
-    */
-   private static Results getResults(String json) {
-      final Gson gson = new Gson();
-      final Results results = gson.fromJson(json, Results.class);
-      return results;
-   }
+	/**
+	 * get the Results object from JSON
+	 */
+	private static Results getResults(String json) {
+		final Gson gson = new Gson();
+		final Results results = gson.fromJson(json, Results.class);
+		return results;
+	}
 
-   public static String lookup(ChromaPrint chromaprint) throws ClientProtocolException, IOException {
-      final Properties properties = new Properties();
-      properties.load(AcoustID.class.getResourceAsStream(PROPERTIES));
-      final String url = properties.getProperty("url") + "?client=" + properties.getProperty("client") + "&meta=recordingids" + "&fingerprint=" + chromaprint.chromaprint + "&duration="
-            + chromaprint.duration;
-      final String json = HTTPUtil.get(url);
-      final Results results = getResults(json);
-      if (results.status.compareTo("ok") == 0) {
-         if ((results.results.size() > 0) && (results.results.get(0).recordings.size() > 0)) {
-            return results.results.get(0).recordings.get(0).id;
-         } else {
-            return null;
-         }
-      } else {
-         return null;
-      }
-   }
+	/**
+	 * do a ChromaPrint lookup and result a musicbrainz id
+	 */
+	public static String lookup(ChromaPrint chromaprint)
+			throws ClientProtocolException, IOException {
+		final Properties properties = new Properties();
+		properties.load(AcoustID.class.getResourceAsStream(PROPERTIES));
+		final String url = properties.getProperty("url") + "?client="
+				+ properties.getProperty("client") + "&meta=recordingids"
+				+ "&fingerprint=" + chromaprint.chromaprint + "&duration="
+				+ chromaprint.duration;
+		final String json = HTTPUtil.get(url);
+		final Results results = getResults(json);
+		/*
+		 * check status
+		 */
+		if (results.status.compareTo("ok") == 0) {
+			/*
+			 * get the best match
+			 */
+			final Result bestResult = getBestResult(results);
+			/*
+			 * return the id
+			 */
+			return bestResult.recordings.get(0).id;
+		} else {
+			return null;
+		}
+	}
 
-   private final static String PROPERTIES = "/acoustid.properties";
+	/**
+	 * get the highest rated result
+	 */
+	private static Result getBestResult(Results results) {
+		Result bestResult = results.results.get(0);
+		int currentScore = Integer.parseInt(bestResult.score);
+		for (Result result : results.results) {
+			int score = Integer.parseInt(result.score);
+			if (score > currentScore) {
+				bestResult = result;
+				currentScore = score;
+			}
+		}
+		return bestResult;
+	}
+
+	private final static String PROPERTIES = "/acoustid.properties";
 }
