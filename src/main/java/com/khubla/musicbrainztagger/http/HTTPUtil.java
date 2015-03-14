9907e53653a8aef508952f08e535b114d8267756
Tom Everett <tom@khubla.com>
2015-03-14 14:21:58 -0600

    add an HTTP User-Agent as required by MusicBrainz

--- src/main/java/com/khubla/musicbrainztagger/http/HTTPUtil.java
+++ src/main/java/com/khubla/musicbrainztagger/http/HTTPUtil.java
@@ -1,6 +1,7 @@
 package com.khubla.musicbrainztagger.http;
 
 import java.io.IOException;
+import java.util.Properties;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -10,27 +11,35 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import com.khubla.musicbrainztagger.musicbrainz.MusicBrainz;
+
 /**
  * @author tom
  */
 public class HTTPUtil {
-   public static class Response {
-      public String response;
-      public int responseCode;
-   }
+	public static class Response {
+		public String response;
+		public int responseCode;
+	}
 
-   public static Response get(String url) throws ClientProtocolException, IOException {
-      final CloseableHttpClient httpclient = HttpClients.createDefault();
-      final HttpGet httpGet = new HttpGet(url);
-      final CloseableHttpResponse httpResponse = httpclient.execute(httpGet);
-      try {
-         final HttpEntity httpEntity = httpResponse.getEntity();
-         final Response ret = new Response();
-         ret.response = EntityUtils.toString(httpEntity);
-         ret.responseCode = httpResponse.getStatusLine().getStatusCode();
-         return ret;
-      } finally {
-         httpResponse.close();
-      }
-   }
+	public static Response get(String url) throws ClientProtocolException,
+			IOException {
+		final Properties properties = new Properties();
+		properties.load(MusicBrainz.class.getResourceAsStream(PROPERTIES));
+		final CloseableHttpClient httpclient = HttpClients.createDefault();
+		final HttpGet httpGet = new HttpGet(url);
+		httpGet.setHeader("User-Agent", properties.getProperty("useragent"));
+		final CloseableHttpResponse httpResponse = httpclient.execute(httpGet);
+		try {
+			final HttpEntity httpEntity = httpResponse.getEntity();
+			final Response ret = new Response();
+			ret.response = EntityUtils.toString(httpEntity);
+			ret.responseCode = httpResponse.getStatusLine().getStatusCode();
+			return ret;
+		} finally {
+			httpResponse.close();
+		}
+	}
+
+	private final static String PROPERTIES = "/musicbrainztagger.properties";
 }
